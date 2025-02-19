fe4e531806e56db3796d00f4ea575dd40e68bc3e
Maxime Robinot <maxime.robinot@dwarfanimation.com>
2025-02-19 11:20:40 +0100

    Fix transfer of override pipeline manifest to running process

--- UnrealEngine5/UnrealEnginePlugins/MoviePipelineDeadline/Content/Python/PreJob.py
+++ UnrealEngine5/UnrealEnginePlugins/MoviePipelineDeadline/Content/Python/PreJob.py
@@ -1,13 +1,19 @@
 import DeadlineUnrealUtils
+from Deadline.Scripting import RepositoryUtils
 
 
 def __main__(*args):
     deadline_plugin = args[0]
 
+    # write the final manifest in the output directory
     manifest_filepath = DeadlineUnrealUtils.write_manifest_file(deadline_plugin, for_cmdline=False)
 
     override_manifest = ""
     with open(manifest_filepath, "r") as manifest:
         override_manifest = manifest.read()
 
-    deadline_plugin.SetProcessEnvironmentVariable("override_serialized_pipeline", override_manifest)
+    # add it to the job's environment variables, so that render tasks can get and use it
+    job = deadline_plugin.GetJob()
+    job.SetJobEnvironmentKeyValue("override_serialized_pipeline", override_manifest)
+
+    RepositoryUtils.SaveJob(job)
